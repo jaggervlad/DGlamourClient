11b579694235b5d58609466182902ecdc00be7bc
Sebastian Acosta <jaggersaa.96@hotmail.com>
2020-07-24 09:56:32 -0500

    Mejoras

--- apollo/index.js
+++ apollo/index.js
@@ -5,12 +5,10 @@ import withApollo from 'next-with-apollo';
 const httpLink = createHttpLink({
   uri: 'https://blooming-citadel-99802.herokuapp.com',
   fetch,
-  credentials: 'include',
 });
 
 export function getStandAloneApollo(initialState = {}) {
   return new ApolloClient({
-    connectToDevTools: true,
     link: httpLink,
     cache: new InMemoryCache().restore(initialState || {}),
   });
