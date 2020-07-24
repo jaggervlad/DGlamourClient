f57fa6e71aec299a4cef689f2c144d7a7164097b
Sebastian Acosta <jaggersaa.96@hotmail.com>
2020-07-24 10:02:02 -0500

    Mejoras

--- apollo/index.js
+++ apollo/index.js
@@ -5,6 +5,7 @@ import withApollo from 'next-with-apollo';
 const httpLink = createHttpLink({
   uri: 'https://blooming-citadel-99802.herokuapp.com',
   fetch,
+  credentials: 'include',
 });
 
 export function getStandAloneApollo(initialState = {}) {
