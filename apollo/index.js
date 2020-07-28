65eff73568c3196f83dbb6f28807d64085bd308c
Sebastian Acosta <jaggersaa.96@hotmail.com>
2020-07-27 20:55:42 -0500

    Al parecer esta ya para production

--- apollo/index.js
+++ apollo/index.js
@@ -5,7 +5,7 @@ import withApollo from 'next-with-apollo';
 const httpLink = createHttpLink({
   uri: 'https://blooming-citadel-99802.herokuapp.com',
   fetch,
-  credentials: 'include',
+  credentials: 'same-origin',
 });
 
 export function getStandAloneApollo(initialState = {}) {
